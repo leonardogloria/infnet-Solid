12e3e93a39ef9c572655c670d7d887da4032ba3d
leonardogloria <contato@leonardogloria.com.br>
2022-05-21 11:24:19 -0300

    Finalizacao SRP Preco

--- src/main/java/br/com/infnet/solid/srp/produto/ReajusteService.java
+++ src/main/java/br/com/infnet/solid/srp/produto/ReajusteService.java
@@ -5,7 +5,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 
 public class ReajusteService {
-    public void reajustaPreco(BigDecimal aumento, Produto produto) throws ValidationException {
+    public void reajustaPreco(BigDecimal aumento, Produto produto)  {
         BigDecimal percentualReajuste = aumento.divide(produto.getPreco(), RoundingMode.HALF_UP);
         if(percentualReajuste.compareTo(new BigDecimal("0.3")) > 0 ){
             throw  new ValidationException("Aumento não pode ser superior a 30%");
