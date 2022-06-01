8334a38b3018931116ab40e7093419ca01388bf4
leonardogloria <contato@leonardogloria.com.br>
2022-06-01 11:46:47 -0300

    First Commit

---
+++ src/main/java/com/example/infinetsolid/InfinetSolidApplication.java
@@ -0,0 +1,13 @@
+package com.example.infinetsolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinetSolidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfinetSolidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infinetsolid/InfinetSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infinetsolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinetSolidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
