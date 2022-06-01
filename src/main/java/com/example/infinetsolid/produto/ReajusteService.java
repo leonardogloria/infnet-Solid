779198fd6925b7a5086eb65879a967e35e527b43
leonardogloria <contato@leonardogloria.com.br>
2022-06-01 14:09:23 -0300

    Finall Commit

--- src/main/java/com/example/infinetsolid/produto/ReajusteService.java
+++ src/main/java/com/example/infinetsolid/produto/ReajusteService.java
@@ -3,6 +3,7 @@ package com.example.infinetsolid.produto;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
     public void reajustaPreco(Produto produto, BigDecimal aumento){
@@ -10,6 +11,13 @@ public class ReajusteService {
         if(percentualAumento.compareTo(new BigDecimal("0.3")) > 0){
             throw new ValidationException("Aumento não pode ser maior que 30%");
         }
+        LocalDate dataUltimoReajuste = produto.getUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if(mesesUltimoReajuste > 3){
+            throw new ValidationException("Ultimo reajusete possui menos de 3 meses.");
+        }
+
         BigDecimal novoPreco = produto.getPreco().add(aumento);
         produto.reajustaPreco(novoPreco);
     }
